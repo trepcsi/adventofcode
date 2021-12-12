59312e78994e20385fb20860fdcfc2e0aeb24e46
Illes Miklos <illes.miklos95@gmail.com>
2021-12-12 15:22:37 +0100

    soolve day12 B

--- src/com/trepcsi/Main.java
+++ src/com/trepcsi/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Solver12 solver12 = new Solver12("./src/com/trepcsi/day12/test.txt");
+        Solver12 solver12 = new Solver12("./src/com/trepcsi/day12/input.txt");
         System.out.println(solver12.solveA());
     }
 }
--- src/com/trepcsi/day12/Solver12.java
+++ src/com/trepcsi/day12/Solver12.java
@@ -3,12 +3,12 @@ package com.trepcsi.day12;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Solver12 {
 
     private Map<String, List<String>> validPathsFromNode = new HashMap<>();
     private int counter = 0;
-    private int smallVisited = 0;
 
     public Solver12(String fileName) {
         try {
@@ -38,35 +38,75 @@ public class Solver12 {
 
     public int solveA() {
         System.out.println(validPathsFromNode);
-        Map<String, Boolean> isVisited = new HashMap<>();
+        Map<String, Integer> isVisited = new HashMap<>();
         List<String> keys = new ArrayList<>(validPathsFromNode.keySet());
         for (String key : keys) {
-            isVisited.put(key, false);
+            isVisited.put(key, 0);
         }
         List<String> localPathList = new ArrayList<>();
         search("START", isVisited, localPathList);
         return counter;
     }
 
-    private void search(String node, Map<String, Boolean> isVisited, List<String> localPathList) {
+    private void search(String node, Map<String, Integer> isVisited, List<String> localPathList) {
         if (node.equals("end")) {
             System.out.println(localPathList);
             counter++;
             return;
         }
-        if (node.equals(node.toLowerCase()) || node.equals("START")) {
-            isVisited.put(node, true);
+        if (node.equals(node.toLowerCase())) {
+            isVisited.put(node, isVisited.get(node) + 1);
         }
         for (int i = 0; i < validPathsFromNode.get(node).size(); i++) {
             String next = validPathsFromNode.get(node).get(i);
-            if (!isVisited.get(next)) {
-                localPathList.add(next);
-                int index = localPathList.size();
-                search(next, isVisited, localPathList);
-                localPathList.remove(index-1);
+            if (!next.equals("START")) {
+                if (next.equals(next.toLowerCase())) {
+                    if (isVisited.get(next) == 1 && noSmallVisitedTwice(isVisited)) {
+                        localPathList.add(next);
+                        int index = localPathList.size();
+                        search(next, isVisited, localPathList);
+                        localPathList.remove(index - 1);
+                    } else if (isVisited.get(next) == 0) {
+                        localPathList.add(next);
+                        int index = localPathList.size();
+                        search(next, isVisited, localPathList);
+                        localPathList.remove(index - 1);
+                    }
+                } else {
+                    localPathList.add(next);
+                    int index = localPathList.size();
+                    search(next, isVisited, localPathList);
+                    localPathList.remove(index - 1);
+                }
             }
         }
-        isVisited.put(node, false);
+        if (node.equals(node.toLowerCase())) {
+            isVisited.put(node, isVisited.get(node) - 1);
+        }
+    }
+
+    private boolean validPath(List<String> localPathList) {
+        int sum = 0;
+        for (int i = 0; i < localPathList.size(); i++) {
+            if (localPathList.get(i).equals(localPathList.get(i).toLowerCase())) {
+                if (Collections.frequency(localPathList, localPathList.get(i)) > 1) {
+                    sum++;
+                }
+            }
+        }
+
+        return sum / 2 < 2;
+    }
+
+    private boolean noSmallVisitedTwice(Map<String, Integer> isVisited) {
+        boolean smallVisitedTwice = false;
+        List<String> keys = new ArrayList<>(isVisited.keySet());
+        for (String key : keys) {
+            if (isVisited.get(key) == 2) {
+                smallVisitedTwice = true;
+            }
+        }
+        return !smallVisitedTwice;
     }
 
 }
