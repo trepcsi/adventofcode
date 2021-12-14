15404c13907b057b07c76ba9222dbe23c98a1e9e
Illes Miklos <illes.miklos95@gmail.com>
2021-12-14 22:59:20 +0100

    before caching

--- src/com/trepcsi/day14/Solver14.java
+++ src/com/trepcsi/day14/Solver14.java
@@ -3,19 +3,16 @@ package com.trepcsi.day14;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class Solver14 {
 
     private String polymer = "";
     private final Map<String, String> reactions = new HashMap<>();
-    private Map<Character, Integer> letterOccurrenceNumber = new HashMap<>();
-    private Map<Character, List<Integer>> letterTendency = new HashMap<>();
+    private HashMap<Character, Long> letterCounterMap = new HashMap<>();
+    private char lastLetter = 'c';
+    private HashMap<Integer, HashMap<String, HashMap<Character, Integer>>> cache = new HashMap<>();
 
     public Solver14(String fileName) {
         try {
@@ -36,78 +33,40 @@ public class Solver14 {
         }
     }
 
-    public int solveA() {
-        for (int j = 0; j < 10; j++) {
-            String newPolymer = "";
-            newPolymer += polymer.substring(0, 1);
-            for (int i = 0; i < polymer.length() - 1; i++) {
-                String key = polymer.substring(i, i + 2);
-                if (reactions.containsKey(key)) {
-                    newPolymer += reactions.get(key);
-                }
-                newPolymer += polymer.substring(i + 1, i + 2);
-            }
-            polymer = newPolymer;
-            fillOccurency();
-            System.out.println(letterOccurrenceNumber);
+    public long solveA() {
+        letterCounterMap.put(polymer.charAt(polymer.length() - 1), 1L);
+        for (int i = 0; i < polymer.length() - 1; i++) {
+            count(polymer.charAt(i), polymer.charAt(i + 1), 0);
         }
-        return getMaxOccurency() - getMinOccurency();
+        return max() - min();
     }
 
+    private long max() {
+        return letterCounterMap.values().stream().max(Long::compare).get();
+    }
 
+    private long min() {
+        return letterCounterMap.values().stream().min(Long::compare).get();
+    }
 
-    private void fillTendency() {
-        List<Character> keys = letterOccurrenceNumber.keySet().stream().toList();
-        for (Character key : keys) {
-            if (letterTendency.containsKey(key)) {
-                letterTendency.get(key).add(letterOccurrenceNumber.get(key));
+    private void count(char x, char y, int i) {
+        if (i == 10) {
+            if (letterCounterMap.containsKey(x)) {
+
+                letterCounterMap.put(x, letterCounterMap.get(x) + 1L);
             } else {
-                List<Integer> value = new ArrayList<>();
-                value.add(letterOccurrenceNumber.get(key));
-                letterTendency.put(key, value);
+                letterCounterMap.put(x, 1L);
             }
+            return;
         }
-    }
+        String key = String.valueOf(x) + String.valueOf(y);
+        if (reactions.get(key) == null) return;
 
-    private void initTendency() {
-        letterTendency = new HashMap<>();
-        List<Character> keys = letterOccurrenceNumber.keySet().stream().toList();
-        for (Character key : keys) {
-            List<Integer> value = new ArrayList<>();
-            value.add(letterOccurrenceNumber.get(key));
-            letterTendency.put(key, value);
-        }
-    }
+        i = i + 1;
 
-    private int getMaxOccurency() {
-        int max = 0;
-        for (var key : letterOccurrenceNumber.keySet()) {
-            if (letterOccurrenceNumber.get(key) > max) {
-                max = letterOccurrenceNumber.get(key);
-            }
-        }
-        return max;
-    }
+        count(x, reactions.get(key).charAt(0), i);
+        count(reactions.get(key).charAt(0), y, i);
 
-    private int getMinOccurency() {
-        int min = 5000;
-        for (var key : letterOccurrenceNumber.keySet()) {
-            if (letterOccurrenceNumber.get(key) < min) {
-                min = letterOccurrenceNumber.get(key);
-            }
-        }
-        return min;
-    }
-
-    private void fillOccurency() {
-        Set<Character> keys = new HashSet<>();
-        for (int i = 0; i < polymer.length(); i++) {
-            keys.add(polymer.charAt(i));
-        }
-        keys.forEach(k -> letterOccurrenceNumber.put(k, 0));
-        for (int i = 0; i < polymer.length(); i++) {
-            letterOccurrenceNumber.put(polymer.charAt(i), letterOccurrenceNumber.get(polymer.charAt(i)) + 1);
-        }
     }
 
 }
